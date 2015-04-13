7c899005472fdefc8486abc2576beee538140ab5
André Girol <andregirol@gmail.com>
2015-04-13 12:16:31 -0300

    Adicionando comentarios e pequenas modificacoes
    Arquivo wordlist small alterado para testes

--- phorca.py
+++ phorca.py
@@ -17,17 +17,15 @@ wordlist = list(fh)
 
 # escolhe palavra aleatoria com a biblioteca random
 palavra = random.choice(wordlist)
+# remove espaços no final do arquivo
 palavra = palavra.strip()
-
-# Inicializa a variavel palavra como uma lista
-#palavra = list()
 
 # variavel para armazenar cada entrada do usuario
 mascara = list()
 
-# adiciona letra a letra na lista
+# adiciona na mascara os underlines para o usuario preencher
 for letra in palavra:
-    #palavra.append(letra)
+
     if letra == '-':
         mascara.append("-")
     else:
@@ -35,35 +33,39 @@ for letra in palavra:
 
 # calcula o tamanho da palavra para dar o número de tentativas
 tentativasMax = int ( len(palavra) + len(palavra)/2 )
+
+# armazena o tamanho para mostrar ao usuário quantas letras
+# possui a palavra
 tamanho = str(len(palavra))
 
 # tentativas do usuario de adivinhar a palavra
 tentativas = 0
 ganhou = False
+
 while tentativas <= tentativasMax:
-    
+    # imprime o banner para o usuario
     banner()
-    
-   # print palavra
+
+    # print palavra
     restantes = tentativasMax - tentativas
-    
+
     charRestantes = str(restantes)
     print "A palavra tem " + tamanho + " letras\n"
     print "Restam ainda " + charRestantes + " tentativas"
     print mascara
     inp = raw_input("\nEntre com uma letra:  ")
     tentativas = tentativas + 1
-    
+
     # Condicoes de saida do programa
-    
-    if inp == "sair": 
+
+    if inp == "sair":
         break
-            
+
     if len(inp) > 1:
         print "Entrada inválida!"
         continue
-    
-    
+
+
     i = 0
     for letra in palavra:
         if inp == letra:
@@ -77,11 +79,11 @@ while tentativas <= tentativasMax:
 banner()
 if ganhou is True:
     print "GANHOU! \o/"
-    
+
 else:
     print "PERDEU... :("
-    
-print "A palavra era: " + palavra
+
+print "\nA palavra era: " + palavra
 
 
 
